d42d8d7f43faf3f082ae197e58edadd673d01e08
unknown <>
2018-03-30 21:55:57 +0800

    import project

---
+++ StealthRecord/Main_From.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace 隐形记录
+{
+    partial class Main_From
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.Tim_Watch = new System.Windows.Forms.Timer(this.components);
+            this.Tim_Exec = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(12, 12);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(107, 12);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "OK";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(12, 41);
+            this.txt.Multiline = true;
+            this.txt.Name = "txt";
+            this.txt.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt.Size = new System.Drawing.Size(170, 72);
+            this.txt.TabIndex = 2;
+            // 
+            // Tim_Watch
+            // 
+            this.Tim_Watch.Interval = 50;
+            this.Tim_Watch.Tick += new System.EventHandler(this.Tim_Watch_Tick);
+            // 
+            // Tim_Exec
+            // 
+            this.Tim_Exec.Interval = 17;
+            this.Tim_Exec.Tick += new System.EventHandler(this.Tim_Exec_Tick);
+            // 
+            // Main_From
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 125);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Main_From";
+            this.Text = "TH";
+            this.Load += new System.EventHandler(this.Main_Form_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_Form_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Timer Tim_Watch;
+        private System.Windows.Forms.Timer Tim_Exec;
+    }
+}
+
---
+++ StealthRecord/Main_From.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 隐形记录
+{
+    public partial class Main_From : Form
+    {
+        public Main_From()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32")]
+        public extern static void SetCursorPos(int x, int y);
+
+        private List<Point> plist = new List<Point>();
+
+        private void Main_Form_Load(object sender, EventArgs e) { }
+
+        private void Main_Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            txt.AppendText(e.KeyCode.ToString());
+            if (e.Alt)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.S:
+                        break;
+                }
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            txt.Clear();
+            plist.Clear();
+            Text = "Start";
+            Tim_Watch.Start();
+        }
+
+        private void Tim_Watch_Tick(object sender, EventArgs e)
+        {
+            plist.Add(Control.MousePosition);
+            Text = plist.Count + "";
+            txt.AppendText(plist.ToArray()[plist.Count - 1].ToString());
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            Tim_Watch.Stop();
+            Tim_Exec.Start();
+        }
+
+        int count = 1;
+
+        private void Tim_Exec_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (count == plist.Count)
+            {
+                Tim_Exec.Stop();
+                Text = "OK";
+                return;
+            } 
+            try
+            {
+                SetCursorPos(plist.ToArray()[count].X, plist.ToArray()[count].Y);
+            }
+            catch { }
+        }
+    }
+}
