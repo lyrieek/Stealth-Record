2307543d88eb3a7d998a12a0771a1456d31176c3
lyrieek <tianhao7@foxmail.com>
2018-03-31 23:05:48 +0800

    split move function

--- StealthRecord/Main_From.Designer.cs
+++ StealthRecord/Main_From.Designer.cs
@@ -30,65 +30,64 @@
         {
             this.components = new System.ComponentModel.Container();
             this.btn_start = new System.Windows.Forms.Button();
-            this.btn_stop = new System.Windows.Forms.Button();
             this.txt = new System.Windows.Forms.TextBox();
             this.Tim_Watch = new System.Windows.Forms.Timer(this.components);
             this.Tim_Exec = new System.Windows.Forms.Timer(this.components);
+            this.btn_exec = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_start
             // 
             this.btn_start.Location = new System.Drawing.Point(12, 12);
             this.btn_start.Name = "btn_start";
-            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.Size = new System.Drawing.Size(90, 23);
             this.btn_start.TabIndex = 0;
             this.btn_start.Text = "Start";
             this.btn_start.UseVisualStyleBackColor = true;
             this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
-            // 
-            // btn_stop
-            // 
-            this.btn_stop.Location = new System.Drawing.Point(107, 12);
-            this.btn_stop.Name = "btn_stop";
-            this.btn_stop.Size = new System.Drawing.Size(75, 23);
-            this.btn_stop.TabIndex = 1;
-            this.btn_stop.Text = "OK";
-            this.btn_stop.UseVisualStyleBackColor = true;
-            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
             // 
             // txt
             // 
             this.txt.Location = new System.Drawing.Point(12, 41);
             this.txt.Multiline = true;
             this.txt.Name = "txt";
-            this.txt.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.txt.Size = new System.Drawing.Size(170, 72);
+            this.txt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt.Size = new System.Drawing.Size(195, 149);
             this.txt.TabIndex = 2;
             // 
             // Tim_Watch
             // 
-            this.Tim_Watch.Interval = 50;
             this.Tim_Watch.Tick += new System.EventHandler(this.Tim_Watch_Tick);
             // 
             // Tim_Exec
             // 
-            this.Tim_Exec.Interval = 17;
             this.Tim_Exec.Tick += new System.EventHandler(this.Tim_Exec_Tick);
+            // 
+            // btn_exec
+            // 
+            this.btn_exec.Enabled = false;
+            this.btn_exec.Location = new System.Drawing.Point(117, 12);
+            this.btn_exec.Name = "btn_exec";
+            this.btn_exec.Size = new System.Drawing.Size(90, 23);
+            this.btn_exec.TabIndex = 0;
+            this.btn_exec.Text = "Exec";
+            this.btn_exec.UseVisualStyleBackColor = true;
+            this.btn_exec.Click += new System.EventHandler(this.btn_exec_Click);
             // 
             // Main_From
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(191, 125);
+            this.ClientSize = new System.Drawing.Size(221, 202);
             this.Controls.Add(this.txt);
-            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_exec);
             this.Controls.Add(this.btn_start);
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "Main_From";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TH";
             this.Load += new System.EventHandler(this.Main_Form_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_Form_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,10 +96,10 @@
         #endregion
 
         private System.Windows.Forms.Button btn_start;
-        private System.Windows.Forms.Button btn_stop;
         private System.Windows.Forms.TextBox txt;
         private System.Windows.Forms.Timer Tim_Watch;
         private System.Windows.Forms.Timer Tim_Exec;
+        private System.Windows.Forms.Button btn_exec;
     }
 }
 
--- StealthRecord/Main_From.cs
+++ StealthRecord/Main_From.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StealthRecord;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,14 +10,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace 隐形记录
-{
-    public partial class Main_From : Form
-    {
-        public Main_From()
-        {
+namespace 隐形记录 {
+    public partial class Main_From : Form {
+        //KeyboardHook k_hook;
+        public Main_From() {
             InitializeComponent();
+            //k_hook = new KeyboardHook();
+            //k_hook.KeyDownEvent += k_hook_KeyDownEvent;
+            //k_hook.Start();
         }
+
+        //void k_hook_KeyDownEvent(object sender, KeyEventArgs e) {
+        //    Output("key:" + e.Alt + "\r\n");
+        //    Output("key:" + e.KeyData + "\r\n");
+        //}
 
         [DllImport("User32")]
         public extern static void SetCursorPos(int x, int y);
@@ -25,56 +32,61 @@ namespace 隐形记录
 
         private void Main_Form_Load(object sender, EventArgs e) { }
 
-        private void Main_Form_KeyDown(object sender, KeyEventArgs e)
-        {
-            txt.AppendText(e.KeyCode.ToString());
-            if (e.Alt)
-            {
-                switch (e.KeyCode)
-                {
-                    case Keys.S:
-                        break;
-                }
+        private void btn_start_Click(object sender, EventArgs e) {
+            if (Tim_Watch.Enabled) {
+                btn_exec.Enabled = true;
+                txt.Clear();
+                btn_start.Text = "Start";
+                Tim_Watch.Stop();
+            } else {
+                btn_exec.Enabled = false;
+                txt.Clear();
+                plist.Clear();
+                btn_start.Text = "Stop";
+                Tim_Watch.Start();
+                Tim_Exec.Stop();
             }
         }
 
-        private void btn_start_Click(object sender, EventArgs e)
-        {
-            txt.Clear();
-            plist.Clear();
-            Text = "Start";
-            Tim_Watch.Start();
-        }
-
-        private void Tim_Watch_Tick(object sender, EventArgs e)
-        {
-            plist.Add(Control.MousePosition);
+        private void Tim_Watch_Tick(object sender, EventArgs e) {
+            var CurrentPoint = Control.MousePosition;
+            if (CurrentPoint.Equals(LastMousePosition)) {
+                return;
+            }
+            plist.Add(CurrentPoint);
             Text = plist.Count + "";
-            txt.AppendText(plist.ToArray()[plist.Count - 1].ToString());
+            Output(CurrentPoint.ToString());
+            LastMousePosition = CurrentPoint;
         }
 
-        private void btn_stop_Click(object sender, EventArgs e)
-        {
-            Tim_Watch.Stop();
-            Tim_Exec.Start();
+        public void Output(string param) {
+            txt.AppendText(param);
         }
 
         int count = 1;
 
-        private void Tim_Exec_Tick(object sender, EventArgs e)
-        {
+        private void Tim_Exec_Tick(object sender, EventArgs e) {
             count++;
-            if (count == plist.Count)
-            {
+            try {
+                if (count >= plist.Count) {
+                    Tim_Exec.Stop();
+                    Text = "OK";
+                    count = 1;
+                    plist.Clear();
+                    return;
+                }
+                Output("{" + plist[count].X + "," + plist[count].Y + "}");
+                SetCursorPos(plist[count].X, plist[count].Y);
+            } catch (Exception ex) {
+                Output("exception:" + ex);
                 Tim_Exec.Stop();
-                Text = "OK";
-                return;
-            } 
-            try
-            {
-                SetCursorPos(plist.ToArray()[count].X, plist.ToArray()[count].Y);
             }
-            catch { }
+        }
+
+        public Point LastMousePosition { get; set; }
+
+        private void btn_exec_Click(object sender, EventArgs e) {
+            Tim_Exec.Start();
         }
     }
 }
